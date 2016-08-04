4696a6a7b48b496682edbe0b910859b4aba7fb85
manu7772 <manu7772@gmail.com>
2016-08-04 16:16:19 +0200

    images du site

--- src/site/UserBundle/Form/Type/ProfileFormType.php
+++ src/site/UserBundle/Form/Type/ProfileFormType.php
@@ -14,7 +14,7 @@ namespace site\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Validator\Constraint\UserPassword as OldUserPassword;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
@@ -153,7 +153,7 @@ class ProfileFormType extends BaseType {
 
 	}
 
-	public function configureOptions(OptionsResolverInterface $resolver) {
+	public function configureOptions(OptionsResolver $resolver) {
 		$resolver->setDefaults(array(
 			'data_class' => 'site\UserBundle\Entity\User',
 			'intention'  => 'profile',
