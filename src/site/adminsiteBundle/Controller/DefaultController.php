af7d5c259e9a6d9576f5c7c9bf0f59b8b4c5e320
manu7772 <manu7772@gmail.com>
2018-05-30 12:41:14 +0200

    Edit intro pageweb's diaporam shortcut on landing admin page

--- src/site/adminsiteBundle/Controller/DefaultController.php
+++ src/site/adminsiteBundle/Controller/DefaultController.php
@@ -13,6 +13,27 @@ use Labo\Bundle\AdminBundle\services\aeData;
  */
 class DefaultController extends extendController {
 	
+	/**
+	 * Page d'accueil adminstration
+	 * @return Response
+	 */
+	public function indexAction() {
+		$data = array();
+		$data['defaultPageweb'] = $this->get(aeData::PREFIX_CALL_SERVICE.'aeServicePageweb')->getDefaultPage();
+		// if site not installed (no default pageweb)
+		if(count($data['defaultPageweb']) < 1) return $this->redirectToRoute('generate');
+		// else…
+		$this->get(aeData::PREFIX_CALL_SERVICE.'aeUrlroutes')->updateBundlesInConfig();
+		$data['sitedata'] = $this->get(aeData::PREFIX_CALL_SERVICE.'aeServiceSite')->getSiteData();
+		$data['messages'] = $this->get(aeData::PREFIX_CALL_SERVICE.'aeServiceMessage')->getRepo()->findNotRead(false);
+		// $data['factures'] = $this->get(aeData::PREFIX_CALL_SERVICE.'aeServiceFacture')->getRepo()->findByState(0);
+		$data['nbmessages'] = count($data['messages']);
+		$nbmessages = $this->getParameter('admin')['accueil_nb_messages'];
+		$data['messages'] = array_slice($data['messages'], 0, $nbmessages); // max. : X messages
+		$data['bundle'] = $this->get(aeData::PREFIX_CALL_SERVICE.'aeUrlroutes')->getBundleName();
+		return $this->render('LaboAdminBundle:Default:index.html.twig', $data);
+	}
+
 
 
 }
